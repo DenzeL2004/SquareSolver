66661197c4e5f584f92b4a7a46f0153c449f92a1
DenzeL2004 <work20022004@mail.ru>
2022-08-13 20:53:49 +0300

    изменения

--- squaresolve.cpp
+++ squaresolve.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <TXLib.h>
-float dis(float a, float b, float c){ // Расчет дескрименанта
+float dis(float a, float b, float c){ //Поиск Дескрименанте
     return b*b - 4*a*c;
 }
-void solves(float a, float b, float D, float &x1, float &x2) { //Поиск корней
+void solves(float a, float b, float D, float &x1, float &x2){ //Поиск корней x1, x2
     x1 = (-b + sqrt(D))/(2*a);
     x2 = (-b - sqrt(D))/(2*a);
 }
 int main(){
-    printf("������� ����������� a, b, c ����������� ��������� ax^2+bx+c = 0\n");
+    printf("Введите коэффиценты a, b, c квадратного уравнения ax^2+bx+c = 0\n");
     float a, b, c;
     scanf("%f%f%f", &a, &b, &c);
 
     float D = dis(a, b, c);
     if(D < 0.0)
-        printf("������� ���\n");
+        printf("Решений нет\n");
     if(D == 0.0){
-        printf("��������� ����� ����� ���� ������\n");
+        printf("Уравнение имеет ровно один корень\n");
         float x1, x2;
         solves(a, b, D, x1, x2);
         printf("x = %.6f", x1);
     }
     if(D > 0.0){
-        printf("��������� ����� ��� �����\n");
+        printf("Уравнение имеет два корня\n");
         float x1, x2;
         solves(a, b, D, x1, x2);
         printf("x1 = %.6f\nx2 = %.6f\n", x1, x2);
