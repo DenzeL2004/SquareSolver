50389067209dc281ddbaf2edac71855377237df4
DenzeL2004 <work20022004@mail.ru>
2022-08-15 17:35:47 +0300

    Проверка на аргументы

--- squaresolve.cpp
+++ squaresolve.cpp
@@ -3,18 +3,19 @@
 #include <TXLib.h>
 #include <assert.h>
 
-enum state {INF_ROOTS = 0, ONE_ROOTS = 1, TWO_ROOTS = 2, NO_ROOTS = 3};
+enum state {INF_ROOTS = 0, LINE = 1, SQUARE = 2, NO_ROOTS = 3, ANOTHER = 4};
 
 int read_arguments (double *a, double *b, double *c){
-    if(scanf ("%lf%lf%lf", a, b, c) != 3){
+    assert (*a != NULL);
+    assert (*b != NULL);
+    assert (*c != NULL);
+
+    if (scanf ("%lf%lf%lf", a, b, c) != 3){
         printf ("Неверный ввод значений\n");
         getchar();
         return 0;
     }
 
-    assert (*a != NULL);
-    assert (*b != NULL);
-    assert (*c != NULL);
     return 1;
 }
 
@@ -26,30 +27,29 @@ char type (double a, double b, double c){
             else
                 return NO_ROOTS;
         else
-            return TWO_ROOTS;
+            return LINE;
     else
-        return ONE_ROOTS;
+        return SQUARE;
+
+    return ANOTHER;
 }
 
 double discriminant (double a, double b, double c){ //поиск дескрименанта
     return b*b - 4*a*c;
 }
 
-void square_roots(double a, double b, double D, double *x1, double *x2){ //поиск корней квадратного уравнения x1, x2
-    *x1 = (-b + sqrt(D))/(2*a);
-    *x2 = (-b - sqrt(D))/(2*a);
-}
-
 void square_solve (double a, double b, double c, double *x1, double *x2){  // Решение квдратного уравнения
     double D = discriminant (a, b, c);
 
-    if(D == 0.0){
-        square_roots (a, b, D, x1, x2);
+    if (D == 0.0){
+        *x1 = -b/(2*a);
         *x2 = NAN;
     }
 
-    if(D > 0.0)
-        square_roots (a, b, D, x1, x2);
+    if (D > 0.0){
+        *x1 = (-b + sqrt(D))/(2*a);
+        *x2 = (-b - sqrt(D))/(2*a);
+    }
 }
 
 void line_solve (double a, double b, double *x){ // решение линейного уравнения
@@ -58,12 +58,12 @@ void line_solve (double a, double b, double *x){ // решение линейного уравнения
 
 void square_solution (double a, double b, double c, double *x1, double *x2, char *T){
     *T = type (a, b, c);
-    switch(*T){
-        case ONE_ROOTS: //Квадратное уравнение
+    switch (*T){
+        case SQUARE: //Квадратное уравнение
             square_solve (a, b, c, x1, x2);
             break;
 
-        case TWO_ROOTS: //Линейное уравнение
+        case LINE: //Линейное уравнение
             line_solve (b, c, x1);
             break;
 
@@ -73,12 +73,12 @@ void square_solution (double a, double b, double c, double *x1, double *x2, char
 }
 
 void write_result (char T, double x1, double x2){
-    switch(T){
+    switch (T){
         case INF_ROOTS:
             printf ("Бесконечное множество решений\n");
             break;
 
-        case ONE_ROOTS:
+        case SQUARE:
             if (isnan(x1) && isnan(x2))
                 printf ("Решений нет\n");
 
@@ -93,7 +93,7 @@ void write_result (char T, double x1, double x2){
             }
             break;
 
-        case TWO_ROOTS:
+        case LINE:
             printf ("Уравнение имеет ровно один корень\n");
             printf ("x = %.6lf\n", x1);
             break;
@@ -117,8 +117,13 @@ int main(){
     if (!read_arguments (&a, &b, &c))
         return 0;
 
+    if (isnan(a) || isnan(b) || isnan(c)){
+        printf("Один из аргументов равен NAN\n");
+        return 0;
+    }
+
     double x1 = NAN, x2 = NAN;
-    char T = NO_ROOTS;
+    char T = ANOTHER;
     square_solution (a, b, c, &x1, &x2, &T);
     write_result (T, x1, x2);
 
