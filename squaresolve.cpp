5b623754747c58fab908a0cc39d7b97d2e3b7be1
DenzeL2004 <work20022004@mail.ru>
2022-08-15 13:39:06 +0300

    Рассмотрел все случаи

--- squaresolve.cpp
+++ squaresolve.cpp
@@ -2,19 +2,64 @@
 #include <math.h>
 #include <TXLib.h>
 
-float dis(float a, float b, float c){ //poisk deskrimenanta
+char type(double a, double b, double c){
+    if(a == 0.0)
+
+        if(b == 0.0)
+
+            if(c == 0.0)
+                return 0;
+            else
+                return 3;
+        else
+            return 2;
+    else
+        return 1;
+}
+
+double dis(double a, double b, double c){ //поиск дескрименанта
     return b*b - 4*a*c;
 }
 
-void solves(float a, float b, float D, float &x1, float &x2){ //poisk korney x1, x2
-    x1 = (-b + sqrt(D))/(2*a);
-    x2 = (-b - sqrt(D))/(2*a);
+void search_solves(double a, double b, double D, double *x1, double *x2){ //поиск корней квадратного уравнения x1, x2
+    *x1 = (-b + sqrt(D))/(2*a);
+    *x2 = (-b - sqrt(D))/(2*a);
+}
+
+void solve1(double a, double b, double c){  // Решение квдратного уравнения
+
+    double D = dis(a, b, c);
+
+    if(D < 0.0)
+        printf("Решений нет\n");
+
+    if(D == 0.0){
+        printf("Уравнение имеет ровно один корень\n");
+        double x1, x2;
+        search_solves(a, b, D, &x1, &x2);
+        printf("x = %.6lf", x1);
+    }
+
+    if(D > 0.0){
+        printf("Уравнение имеет два корня\n");
+        double x1, x2;
+        search_solves(a, b, D, &x1, &x2);
+        printf("x1 = %.6lf\nx2 = %.6lf\n", x1, x2);
+    }
+}
+
+void solve2(double a, double b){ // решение линейного уравнения
+    double x;
+    x = 0.0;
+    x = -b/a;
+    printf("Уравнение имеет ровно один корень\n");
+    printf("x = %.6lf", x);
 }
 
 int main(){
     printf("Введите коэффиценты a, b, c квадратного уравнения ax^2+bx+c = 0\n");
 
-    float a, b, c;
+    double a, b, c;
     a = b = c = 0.0;
 
     if(scanf("%lf%lf%lf", &a, &b, &c) != 3){
@@ -23,25 +68,26 @@ int main(){
         return 0;
     }
 
-    float D = dis(a, b, c);
+    switch(type(a, b, c)){
+        case 0: //Тривиальное уравнение
+            printf("Бесконечное множество решений\n");
+            break;
+        case 1: //Квадратное уравнение
+            solve1(a, b, c);
+            break;
+        case 2: //Линейное уравнение
+            solve2(b, c);
+            break;
+        case 3: //Решений нет
+            printf("Решение нет\n");
+            break;
+        default:
+            printf("ERROR\n");
+            break;
 
-    if(D < 0.0)
-        printf("Решений нет\n");
-
-    if(D == 0.0){
-        printf("Уравнение имеет ровно один корень\n");
-        float x1, x2;
-        solves(a, b, D, x1, x2);
-        printf("x = %.6f", x1);
-    }
-
-    if(D > 0.0){
-        printf("Уравнение имеет два корня\n");
-        float x1, x2;
-        solves(a, b, D, x1, x2);
-        printf("x1 = %.6f\nx2 = %.6f\n", x1, x2);
     }
 
     getchar();
     return 0;
 }
+
