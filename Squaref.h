926f4c37ec610ff9adbf4235ba8c0b20ee7ee436
DenzeL2004 <work20022004@mail.ru>
2022-08-15 20:25:45 +0300

    Проверка на 0 double

--- Squaref.h
+++ Squaref.h
@@ -5,3 +5,4 @@ char square_solve (double a, double b, double c, double *x1, double *x2);
 char line_solve (double a, double b, double *x);
 char count_solves (double a, double b, double c, double *x1, double *x2, char *T);
 void write_result (char T, double x1, double x2);
+bool is_zero (double n);
--- squaresolve.cpp
+++ squaresolve.cpp
@@ -4,79 +4,24 @@
 #include <assert.h>
 #include "Squaref.h"
 
-const long double eps = 1e-9;
-enum state {INF_ROOTS = 0, LINE = 1, SQUARE = 2, NO_ROOTS = 3, ANOTHER = 4};
+const long double EPS = 1e-9;
+enum state {INF_ROOTS = -1, NO_ROOTS = 0, ONE_ROOT = 1, SQUARE = 2, ANOTHER = 3};
 
-int read_arguments (double *a, double *b, double *c){
-    assert (a != NULL);
-    assert (b != NULL);
-    assert (c != NULL);
+int main(){
+    printf ("Введите коэффиценты a, b, c квадратного уравнения ax^2+bx+c = 0\n");
 
-    if (scanf ("%lf%lf%lf", a, b, c) != 3){
-        printf ("Неверный ввод значений\n");
-        getchar();
-        return 0;
-    }
+    double a = NAN, b = NAN, c = NAN;
 
-    return 1;
-}
-
-char type (double a, double b, double c){
-    assert (!isnan(a));
-    assert (!isnan(b));
-    assert (!isnan(c));
-
-    if (fabs(a) < eps)
-        if (fabs(b) < eps)
-            if (fabs(c) < eps)
-                return INF_ROOTS;
-            else
-                return NO_ROOTS;
-        else
-            return LINE;
-    else
-        return SQUARE;
-
-    return ANOTHER;
-}
-
-double discriminant (double a, double b, double c){ //поиск дескрименанта
-    assert (!isnan(a));
-    assert (!isnan(b));
-    assert (!isnan(c));
-
-    return b*b - 4*a*c;
-}
-
-char square_solve (double a, double b, double c, double *x1, double *x2){  // Решение квдратного уравнения
-    assert (!isnan(a));
-    assert (!isnan(b));
-    assert (!isnan(c));
-
-    double D = discriminant (a, b, c);
-
-    if(D < -eps)
+    if (!read_arguments (&a, &b, &c))
         return 0;
 
-    if (fabs(D) < eps){
-        *x1 = -b/(2*a);
-        return 1;
-    }
+    double x1 = NAN, x2 = NAN;
+    char T = ANOTHER, count_roots = 0;
+    count_roots = count_solves (a, b, c, &x1, &x2, &T);
+    write_result (count_roots, x1, x2);
 
-    if (D > eps){
-        *x1 = (-b + sqrt(D))/(2*a);
-        *x2 = (-b - sqrt(D))/(2*a);
-        return 2;
-    }
-}
-
-char line_solve (double a, double b, double *x){ // решение линейного уравнения
-    assert (!isnan(a));
-    assert (!isnan(b));
-
-    *x = -b/a;
-
-    return 1;
+    getchar();
+    return 0;
 }
 
 char count_solves (double a, double b, double c, double *x1, double *x2, char *T){
@@ -92,63 +37,120 @@ char count_solves (double a, double b, double c, double *x1, double *x2, char *T
         case SQUARE: //Квадратное уравнение
             return square_solve (a, b, c, x1, x2);
 
-        case LINE: //Линейное уравнение
+        case ONE_ROOT: //Линейное уравнение
             return line_solve (b, c, x1);
 
         case NO_ROOTS:
             return 0;
 
         default:
-            return CHAR_MIN;
+            return ANOTHER;
     }
 }
 
-void write_result (char cnt, double x1, double x2){
-    if (cnt == -1){
+char type (double a, double b, double c){
+    assert (!isnan(a));
+    assert (!isnan(b));
+    assert (!isnan(c));
+
+    if (is_zero(a))
+        if (is_zero(b))
+            if (is_zero(c))
+                return INF_ROOTS;
+            else
+                return NO_ROOTS;
+        else
+            return ONE_ROOT;
+    else
+        return SQUARE;
+
+    return ANOTHER;
+}
+
+char square_solve (double a, double b, double c, double *x1, double *x2){  // Решение квдратного уравнения
+    assert (!isnan(a));
+    assert (!isnan(b));
+    assert (!isnan(c));
+
+    double D = discriminant (a, b, c);
+
+    if (is_zero(D)){
+        *x1 = -b/(2*a);
+        return 1;
+    }
+
+    if(D < 0.0)
+        return 0;
+
+    if (D > 0.0){
+        *x1 = (-b + sqrt(D))/(2*a);
+        *x2 = (-b - sqrt(D))/(2*a);
+        return 2;
+    }
+}
+
+double discriminant (double a, double b, double c){ //поиск дескрименанта
+    assert (!isnan(a));
+    assert (!isnan(b));
+    assert (!isnan(c));
+
+    return b*b - 4*a*c;
+}
+
+char line_solve (double a, double b, double *x){ // решение линейного уравнения
+    assert (!isnan(a));
+    assert (!isnan(b));
+
+    *x = -b/a;
+
+    return 1;
+}
+
+int read_arguments (double *a, double *b, double *c){ //считывание значений
+    assert (a != NULL);
+    assert (b != NULL);
+    assert (c != NULL);
+
+    if (scanf ("%lf%lf%lf", a, b, c) != 3){
+        printf ("Неверный ввод значений\n");
+        getchar();
+        return 0;
+    }
+
+    return 1;
+}
+
+void write_result (char count_roots, double x1, double x2){ //Вывод кол-ва корней
+    if (count_roots == INF_ROOTS){
         printf ("Бесконечное множество решений\n");
         return;
     }
 
-    if (cnt == 0){
+    if (count_roots == NO_ROOTS){
         printf ("Решений нет\n");
         return;
     }
 
-    if (cnt == 1){
+    if (count_roots == ONE_ROOT){
         printf ("Уравнение имеет роано одно решение\n");
 
-        if(fabs(x1) < eps)
+        if (is_zero(x1))
             x1 = -x1;
 
         printf ("x = %.6lf\n", x1);
         return;
     }
 
-    if(cnt == 2){
+    if (count_roots == SQUARE){
         printf ("Уравнение имеет два различных решения\n");
         printf ("x1 = %.6lf\nx2 = %.6lf\n", x1, x2);
         return;
     }
 
-    printf("ERROR\n");
+    printf ("ERROR\n");
     return;
-
 }
 
-int main(){
-    printf ("Введите коэффиценты a, b, c квадратного уравнения ax^2+bx+c = 0\n");
-
-    double a = NAN, b = NAN, c = NAN;
-
-    if (!read_arguments (&a, &b, &c))
-        return 0;
-
-    double x1 = NAN, x2 = NAN;
-    char T = ANOTHER, cnt = 0;
-    cnt = count_solves (a, b, c, &x1, &x2, &T);
-    write_result (cnt, x1, x2);
-
-    getchar();
-    return 0;
+bool is_zero (double n){
+    return (n == 0.0) || (fabs(n) < EPS);
 }
-
